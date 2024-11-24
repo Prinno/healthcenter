52d4e06dc2c944e9d87b3b326c374bbdbb2e7798
INNOCENT  PHILBERT <69362339+Prinno@users.noreply.github.com>
2024-11-24 22:59:17 +0300

    Update urls.py

--- health/urls.py
+++ health/urls.py
@@ -18,12 +18,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('users/', include('users.urls')),
+    path("users/", include("users.urls")),
+    # Redirect root URL to admin
+    path("", RedirectView.as_view(url="/admin/", permanent=False)),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+
